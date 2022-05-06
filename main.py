19f5b9928b7b96c4e28f237122924abcc036b536
Łukasz <elek123456789101112@gmail.com>
2022-05-07 01:44:06 +0200

    Rewrote the game using objects

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import copy as copy
 import random
+import sys
 import time
 
 import numpy as np
@@ -13,155 +14,17 @@ def snek():
     x_size = 16
     y_size = 16
     screen_size = [y_size * tile_size, x_size * tile_size]
-    ascii_graphics = [""]
-    board = np.full((y_size, x_size), ascii_graphics[0])
-    snake_pos = [random.randrange(0, x_size - 1), random.randrange(0, y_size - 1)]
-    board[snake_pos[1], snake_pos[0]] = "s"
-    body = [[-1, -1]]
-    snek_rotation = 0
-    fruit = [1, 1]
-    direction = 0
     speed = 150
-    speed_mode = 0
-
-    # 1 = x, 0 = y
-    def screen_set_hehe(x, y, z):
-        for c in range(0, z):
-            screen.set_at((x + c, y), "BLACK")
-
-    def game_over():
-        pg.mixer.music.fadeout(500)
-        pg.mixer.fadeout(500)
-        def animation():
-            falling = pg.mixer.Sound('../Snake-AI/sounds/fall.mp3')
-            wasted = pg.mixer.Sound('../Snake-AI/sounds/wasted.mp3')
-            z = 25
-            falling.play()
-            time.sleep(0.5)
-            for x in range(0, int(screen_size[1]), z):
-                for y in range(0, screen_size[0]):
-                    screen_set_hehe(x, y, z)
-                    if y % 20 == 0:
-                        pg.display.update()
-                pg.display.update()
-            pg.mixer.fadeout(100)
-            wasted.play()
-            time.sleep(1)
-            for x in range(0, 500):
-                font = pg.font.SysFont('arial', int(tile_size * x_size / (len('GAME OVER') - 1)))
-                fotn = font.render('GAME OVER', True, 'WHITE')
-                q, w = pg.font.Font.size(font, 'GAME OVER')  # dimensions of text object
-                screen.fill((0, 0, 0))
-                screen.blit(fotn, (
-                    int(screen_size[1] / 10) + random.randrange(-10, 10),
-                    int(screen_size[0] / 2) + random.randrange(-10, 10)))
-                pg.display.update()
-        #animation()
-        return False
-
-    def collision_body(body, snake_pos):
-        if len(body) > 0:
-            for x in range(1, len(body)):
-                # print([snake_pos, body[x]])
-                if body[x] == snake_pos:
-                    print("gae over")
-                    return 1
-
-    def collision_fruit(fruit, snake_pos):
-        if snake_pos == fruit:
-            body.append([-1, -1])
-            fruit_sound.play()
-            fruit = generate_fruit(fruit)
-        return fruit
-
-    def generate_fruit(fruit):
-        fruit = [random.randrange(0, x_size), random.randrange(0, y_size)]
-        for x in body:
-            if x == fruit:
-                fruit = generate_fruit(fruit)
-        return fruit
-
-    def correction():
-        if snake_pos[1] > y_size - 1:
-            snake_pos[1] = 0
-        if snake_pos[1] < 0:
-            snake_pos[1] = y_size - 1
-
-        if snake_pos[0] > x_size - 1:
-            snake_pos[0] = 0
-        if snake_pos[0] < 0:
-            snake_pos[0] = x_size - 1
-
-    def direction_module(direction, snake_pos, body):
-        if direction == 1:
-            snake_pos[0] = snake_pos[0] - 1
-        if direction == 2:
-            snake_pos[0] = snake_pos[0] + 1
-        if direction == 3:
-            snake_pos[1] = snake_pos[1] - 1
-        if direction == 4:
-            snake_pos[1] = snake_pos[1] + 1
-        correction()
-        body_update(body, snake_pos)
-
-    def movement(snek, snek_rotation, body, snake_pos, direction):
-        if keys[0] == 'a' and direction != 2:
-            direction = 1
-            snek = pg.transform.rotate(snek, snek_rotation - 270)
-            snek_rotation = 270
-            return [snek, snek_rotation, direction]
-        if keys[0] == 'd' and direction != 1:
-            direction = 2
-            snek = pg.transform.rotate(snek, snek_rotation - 90)
-            snek_rotation = 90
-            return [snek, snek_rotation, direction]
-        if keys[0] == 'w' and direction != 4:
-            direction = 3
-            snek = pg.transform.rotate(snek, snek_rotation)
-            snek_rotation = 0
-            return [snek, snek_rotation, direction]
-        if keys[0] == 's' and direction != 3:
-            direction = 4
-            snek = pg.transform.rotate(snek, snek_rotation - 180)
-            snek_rotation = 180
-            return [snek, snek_rotation, direction]
-        if len(keys) > 1:
-            keys.pop(0)
-            movement(snek, snek_rotation, body, snake_pos, direction)
-        return [snek, snek_rotation, direction]
-
-    def body_update(body, snake_pos):
-        # print(len(body))
-        if len(body) > 0:
-            for x in range(len(body) - 1, -1, -1):
-                # print(x)
-                body[x] = body[x - 1]
-        # print(body)
-        body[0] = copy.copy(snake_pos)
-
-    pg.init()
     screen = pg.display.set_mode((screen_size[1], screen_size[0]))
     pg.display.set_caption("snek")
     background = pg.image.load('../Snake-AI/sprites/white.png').convert()
     background = pg.transform.scale(background, (tile_size, tile_size))
-    snek = pg.image.load('../Snake-AI/sprites/snek.png').convert()
-    snek = pg.transform.scale(snek, (tile_size, tile_size))
-    body_sprite = pg.image.load('../Snake-AI/sprites/body.png').convert()
-    body_sprite = pg.transform.scale(body_sprite, (tile_size, tile_size))
-    fruit = generate_fruit(fruit)
-    fruit_sprite = pg.image.load('../Snake-AI/sprites/fruit.png')
-    fruit_sprite = pg.transform.scale(fruit_sprite, (tile_size, tile_size))
-    #pg.mixer.music.load('../Snake-AI/sounds/music.mp3')
-    #pg.mixer.music.set_volume(0.1)
-    #pg.mixer.music.play()
-
-    fruit_sound = pg.mixer.Sound('../Snake-AI/sounds/fruit.mp3')
-    fruit_sound.set_volume(0.3)
+    number_of_fruits = 1
     keys = []
     running = True
-    while running:
-        startTime = time.time()
-        screen.fill((0, 0, 0))
+    pg.init()
+
+    def get_input(running):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -174,45 +37,146 @@ def snek():
                     keys.append('d')
                 if event.key == pg.K_a:
                     keys.append('a')
-        if len(keys) >= 1:
-            #print(len(keys))
-            a = movement(snek, snek_rotation, body, snake_pos, direction)
-            snek = a[0]
-            snek_rotation = a[1]
-            direction = a[2]
-            keys.pop(0)
-        direction_module(direction, snake_pos, body)
-        fruit = collision_fruit(fruit, snake_pos)
-        # print(board)
-        board = np.full((screen_size[1], screen_size[0]), ascii_graphics[0])
-        board[snake_pos[1], snake_pos[0]] = "s"
-        board[fruit[1], fruit[0]] = "f"
-        if len(body) > 0:
-            for x in range(0, len(body) - 1):
-                board[body[x + 1][1], body[x + 1][0]] = "b"
-        for x in range(0, x_size):
-            for y in range(0, y_size):
-                screen.blit(background, (x * tile_size, y * tile_size))
-                if board[y][x] == "s":
-                    screen.blit(snek, (x * tile_size, y * tile_size))
-                if board[y][x] == "b":
-                    screen.blit(body_sprite, (x * tile_size, y * tile_size))
-                if board[y][x] == "f":
-                    screen.blit(fruit_sprite, (x * tile_size, y * tile_size))
+        return running
+
+    def draw_score():
         font = pg.font.SysFont(None, 16 * scale)
-        img = font.render(str(len(body)), True, 'BLACK')
+        img = font.render(str(len(snake.body)), True, 'BLACK')
         screen.blit(img, (20, 20))
+
+    class Fruit:
+        def __init__(self):
+            self.location = [-1, -1]
+            self.sprite = pg.image.load('../Snake-AI/sprites/fruit.png')
+            self.sprite = pg.transform.scale(self.sprite, (tile_size, tile_size))
+            self.sound = pg.mixer.Sound('../Snake-AI/sounds/fruit.mp3')
+            self.sound.set_volume(0.3)
+
+        def generate(self, snake_body):
+            self.location = [random.randrange(0, x_size), random.randrange(0, y_size)]
+            for body_part in snake_body:
+                if body_part == self.location:
+                    self.location = self.generate(snake_body)
+            return self.location
+
+        def draw(self):
+            screen.blit(self.sprite, (self.location[0] * tile_size, self.location[1] * tile_size))
+
+    class Snake:
+        def __init__(self):
+            self.pos_x = random.randrange(0, x_size - 1)
+            self.pos_y = random.randrange(0, y_size - 1)
+            self.body = [[-1, -1]]
+            self.head_sprite = pg.image.load('../Snake-AI/sprites/snek.png').convert()
+            self.head_sprite = pg.transform.scale(self.head_sprite, (tile_size, tile_size))
+            self.body_sprite = pg.image.load('../Snake-AI/sprites/body.png').convert()
+            self.body_sprite = pg.transform.scale(self.body_sprite, (tile_size, tile_size))
+            self.rotation = 0
+            self.direction = 0
+            self.is_alive = True
+
+        def movement(self):
+            if self.direction == 1:
+                self.pos_x = self.pos_x - 1
+            if self.direction == 2:
+                self.pos_x = self.pos_x + 1
+            if self.direction == 3:
+                self.pos_y = self.pos_y - 1
+            if self.direction == 4:
+                self.pos_y = self.pos_y + 1
+
+        def pos_correction(self):
+            if self.pos_y > y_size - 1:
+                self.pos_y = 0
+            if self.pos_y < 0:
+                self.pos_y = y_size - 1
+
+            if self.pos_x > x_size - 1:
+                self.pos_x = 0
+            if self.pos_x < 0:
+                self.pos_x = x_size - 1
+
+        def collision_body(self):
+            if len(self.body) > 0:
+                for body_part in range(1, len(self.body)):
+                    if self.body[body_part] == [self.pos_x, self.pos_y]:
+                        self.is_alive = False
+
+        def collision_fruit(self, Fruit):
+            if [self.pos_x, self.pos_y] == Fruit.location:
+                self.body.append([-1, -1])
+                Fruit.sound.play()
+                Fruit.location = Fruit.generate(self.body)
+            return Fruit.location
+
+        def tail_update(self):
+            if len(self.body) > 0:
+                for body_part in range(len(self.body) - 1, -1, -1):
+                    self.body[body_part] = self.body[body_part - 1]
+            self.body[0] = copy.copy([self.pos_x, self.pos_y])
+
+        def handle_input(self, keys):
+            if keys[0] == 'a' and self.direction != 2:
+                self.direction = 1
+                self.head_sprite = pg.transform.rotate(self.head_sprite, self.rotation - 270)
+                self.rotation = 270
+                return
+            if keys[0] == 'd' and self.direction != 1:
+                self.direction = 2
+                self.head_sprite = pg.transform.rotate(self.head_sprite, self.rotation - 90)
+                self.rotation = 90
+                return
+            if keys[0] == 'w' and self.direction != 4:
+                self.direction = 3
+                self.head_sprite = pg.transform.rotate(self.head_sprite, self.rotation)
+                self.rotation = 0
+                return
+            if keys[0] == 's' and self.direction != 3:
+                self.direction = 4
+                self.head_sprite = pg.transform.rotate(self.head_sprite, self.rotation - 180)
+                self.rotation = 180
+                return
+
+        def draw(self):
+            if len(self.body) > 0:
+                for body_part in range(1, len(self.body)):
+                    screen.blit(self.body_sprite,
+                                (self.body[body_part][0] * tile_size, self.body[body_part][1] * tile_size))
+            screen.blit(self.head_sprite, (self.pos_x * tile_size, self.pos_y * tile_size))
+
+        def update(self, keys):
+            if len(keys) >= 1:
+                self.handle_input(keys)
+            self.movement()
+            self.pos_correction()
+            self.tail_update()
+            self.collision_body()
+
+    snake = Snake()
+    fruits = {}
+    for x in range(0, number_of_fruits):
+        fruits['fruit ' + str(x)] = Fruit()
+        fruits['fruit ' + str(x)].generate(snake.body)
+
+    while running:
+        start_time = time.time()
+        screen.fill((255, 255, 255))
+        running = get_input(running)
+        snake.update(keys)
+        if len(keys) > 1:
+            keys.pop(0)
+        snake.draw()
+        for x in range(0, number_of_fruits):
+            fruits['fruit ' + str(x)].location = snake.collision_fruit(fruits['fruit ' + str(x)])
+            fruits['fruit ' + str(x)].draw()
+
+        draw_score()
         pg.display.update()
-        if collision_body(body, snake_pos) == 1:
-            running = game_over()
-        # if speed < 100:
-        #    speed_mode = 1
-        #    speed = 102
-        # if speed_mode == 0:
-        #    speed = speed - 1.5
-        # else:
-        #    speed = speed - 1.5
-        pg.time.delay(int(speed - (time.time() - startTime)))
+        # if collision_body(body, snake_pos) == 1:
+        #     running = False
+        if not snake.is_alive:
+            running = False
+        pg.time.delay(int(speed - (time.time() - start_time)))
 
 
 snek()
